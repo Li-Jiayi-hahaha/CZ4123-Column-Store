81b77271be2965b92ad88de2101d37fd284cb44b
Reeves Chiu <reeveschiu@gmail.com>
2023-04-06 13:55:08 +0800

    old db codes

--- JavaProj/src/App.java
+++ JavaProj/src/App.java
@@ -1,69 +1,61 @@
 import java.util.*;
 
+
+import javax.tools.DocumentationTool.Location;
+
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            App app = new App();
-            app.displayMenu(1);
+            DB db = new DB();
+            db.readCSV();
+            db.printArray("Changi");
+            // db.printArray("Paya Lebar");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    public void displayMenu(int type) throws Exception {
+    // public void displayMenu(int type) throws Exception {
 
-        if (type == 1) {
-            System.out.println("======================================================================================");
-            System.out.println("            << Welcome to Group X Project 1 >>");
-            System.out.println();
-            System.out.println("What would you like to do?");
-            System.out.println("1) Initialize DB \n2) Exit");
-            System.out.println("======================================================================================");
-            System.out.print("You have selected: ");
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
+    //     if (type == 1) {
+    //         System.out.println("======================================================================================");
+    //         System.out.println("            << Welcome to Group X Project 1 >>");
+    //         System.out.println();
+    //         System.out.println("What would you like to do?");
+    //         System.out.println("1) Initialize DB \n2) Exit");
+    //         System.out.println("======================================================================================");
+    //         System.out.print("You have selected: ");
+    //         Scanner in = new Scanner(System.in);
+    //         String input = in.nextLine();
 
-            switch (input) {
-                case "1":
-                    DB db = new DB();
-                    db.readCSV();
-                    break;
-                case "2":
-                    System.exit(0);
-            }
+    //         switch (input) {
+    //             case "1":
+    //                 DB db = new DB();
+    //                 db.readCSV();
+    //                 break;
+    //             case "2":
+    //                 System.exit(0);
+    //         }
             
-        } else {
-            String input;
-            do {
-                System.out.println("======================================================================================");
-                System.out.println("Enter Metric Number");
-                System.out.println("(exit): Exit ");
-                System.out.println("======================================================================================");
-                System.out.print("Selection: ");
-                try (Scanner in = new Scanner(System.in)) {
-                    input = in.nextLine();
-                }
+    //     } else {
+    //         String input;
+    //         do {
+    //             System.out.println("======================================================================================");
+    //             System.out.println("Enter Metric Number");
+    //             System.out.println("(exit): Exit ");
+    //             System.out.println("======================================================================================");
+    //             System.out.print("Selection: ");
+    //             try (Scanner in = new Scanner(System.in)) {
+    //                 input = in.nextLine();
+    //             }
 
-            } while (!input.equals("exit"));
+    //         } while (!input.equals("exit"));
 
-        }
-
-
-    }
+    //     }
 
 
-
-
-
-
-
-
-
-
-
-
-
+    // }
 
 }
 
--- JavaProj/src/DB.java
+++ JavaProj/src/DB.java
@@ -7,14 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DB {
+
+    private List<String[]> sortedData;
+    private List<String[]> dataChangi;
+    private List<String[]> dataPayaLebar;
+
+    public DB() {
+        this.dataChangi = new ArrayList<>();
+        this.dataPayaLebar = new ArrayList<>();
+        this.sortedData = new ArrayList<>();
+    }
     
     public void readCSV(){
         String csvFile = "SingaporeWeather.csv";
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-
-        List<String[]> data = new ArrayList<>();
 
         try {
 
@@ -24,14 +32,30 @@ public class DB {
                 // use comma as separator
                 String[] row = line.split(cvsSplitBy);
 
-                // Process row data
-                for (String value : row) {
-                    System.out.print(value + ", ");
+                //Simple Sorting
+                if (row[2].equals("Changi")){
+                    dataChangi.add(row);
+                    // System.out.println("C ADDED");
                 }
-                System.out.println();
-                data.add(row);
+                if (row[2].equals("Paya Lebar")){
+                    dataPayaLebar.add(row);
+                    // System.out.println("P ADDED");
+                }
+
+                // // Process row data
+                // for (String value : row) {
+                //     System.out.print(value + ", ");
+                // }
+                // System.out.println();
 
             }
+            // for (String[] row : data){
+            //     System.out.println("=============================== DATA ===================================");
+            //     for (String value : row) {
+            //         System.out.print(value + ", ");
+            //     }
+            //     System.out.println();
+            // }
 
         }catch (IOException e) {
             e.printStackTrace();
@@ -44,11 +68,69 @@ public class DB {
                 }
             }
         }
+    }
 
 
+    public void sortArray(List<String[]> Array){
+        //2003-02-02 00:40
+        //2002-01-01 00:30
 
+        //0 year - 2021
+        //1 month - 12
+        //2 date - 31
+        //3 hour - 23
+        //4 minutes - 30
+        String[] date;
+        String[] temp;
+        List<String[]> data = new ArrayList<>();
 
+        date = splitDate((Array.get(1))[1]);
+        for ( String[] row : Array ){
+            temp = splitDate(row[1]);
+            if(Integer.parseInt(date[0]) <= Integer.parseInt(temp[0])){
+                if(Integer.parseInt(date[1]) <= Integer.parseInt(temp[1])){
+                    if(Integer.parseInt(date[2]) <= Integer.parseInt(temp[2])){
+                        if(Integer.parseInt(date[3]) <= Integer.parseInt(temp[3])){
+                            break;
+                        }
+                    }
+                }
+            }
+            data.add(row);
+        }
+    }
 
+    public String[] splitDate(String date){
+        String[] parts = date.split("[-| |:]");
+
+        for (String part : parts) {
+            System.out.println(part);
+        }
+        
+        return parts;
+    }
+
+    public void printArray(String location){
+        System.out.println(dataChangi.get(1));
+        if(location == "Changi"){
+            System.out.println("=============================== Changi ===================================");
+            for ( String[] row : dataChangi ){
+                for (String value : row) {
+                    System.out.print(value + ", ");
+                }
+                System.out.println();
+            }
+
+        }
+        else {
+            System.out.println("=============================== PAYA ===================================");
+            for ( String[] row : dataPayaLebar ){
+                for (String value : row) {
+                    System.out.print(value + ", ");
+                }
+                System.out.println();
+            }
+        }
     }
     
 }
